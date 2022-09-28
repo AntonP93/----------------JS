20bab1804d57ecc61fc6bed5d6a3fa74f0cf673f
Anton Plastun <p.anton070793@gmail.com>
2022-09-28 22:01:08 +0300

    file: add new func

--- index.js
+++ index.js
@@ -230,3 +230,58 @@
 // }
 
 // </script> */}
+
+// получение целевого элемента события
+// eo - объект события
+// function getEventElement(eo) {
+//     if ( window.event && window.event.srcElement )
+//       return window.event.srcElement;
+  
+//     if ( eo.target )
+//       return eo.target;
+  
+//     return null;
+//   }
+  
+//   // остановка распространения события
+//   // eo - объект события
+//   function stopPropagation(eo) {
+//     if ( eo.stopPropagation )
+//       eo.stopPropagation();
+//     else
+//       eo.cancelBubble=true;
+//   }
+  
+//   // отмена обработки события по умолчанию
+//   // eo - объект события
+//   function preventDefault(eo) {
+//     if ( eo.preventDefault )
+//       eo.preventDefault(); 
+//     else
+//       eo.returnValue=false;
+//   }
+  
+//   // получение нажатой кнопки мыши
+//   // eo - объект события
+//   function getMouseWhich(eo) {
+//     if ( eo.which ) return eo.which;
+//     if ( eo.button&1 ) return 1;
+//     if ( eo.button&4 ) return 2;
+//     if ( eo.button&2 ) return 3;
+//     return 0;
+//   }
+  
+//   // получение нажатого на клавиатуре символа
+//   // eo - объект события
+//   function getKeyboardChar(eo) {
+//     if ( eo.which==null ) {  // IE
+//       if ( eo.keyCode<32) return null; // управляющая клавиша
+//       return String.fromCharCode(eo.keyCode); // печатный символ
+//     }
+//     if ( eo.which!=0 && eo.charCode!=0 ) {  // остальные браузеры
+//       if ( eo.which<32 ) return null; // управляющая клавиша
+//       return String.fromCharCode(eo.which); // печатный символ
+//     }
+//     return null; // управляющая клавиша
+//   }
+  
